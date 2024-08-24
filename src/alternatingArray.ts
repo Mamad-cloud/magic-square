838519c4706bd7df2ce6f0ea45bfc27a1ebbd7a4
Fatahi Muhamad <fatahimohamad877@gmail.com>
2024-08-25 02:32:35 +0330

    added not working image steganography example

--- src/alternatingArray.ts
+++ src/alternatingArray.ts
@@ -6,8 +6,6 @@ function createAlternatingArray(size: number, start: number = 1) : Array<Int32Ar
     for ( let i = 0; i < size; i++) 
         arr[i] = new Int32Array(size).fill(0)
 
-    console.log(arr)
-
     for (let i = 0; i < size; i++) {
         if (i % 2 == 1) {
             for (let j = size - 1; j >= 0; j--) {
---
+++ src/imageSteg.ts
@@ -0,0 +1,177 @@
+const imageElem = <HTMLImageElement>document.createElement('img')
+imageElem.src = 'image.png'
+let ar = imageElem.width / imageElem.height
+
+const canvas = document.createElement('canvas') as HTMLCanvasElement
+canvas.width = 800 
+canvas.height = 800 * (1 / ar)
+let ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+
+const canvas2 = document.createElement('canvas') as HTMLCanvasElement
+canvas2.width = 800 
+canvas2.height = 800 * (1 / ar)
+let ctx2 = canvas2.getContext('2d') as CanvasRenderingContext2D
+
+document.body.appendChild(canvas)
+document.body.appendChild(canvas2)
+
+imageElem.onload = (_ev) => {
+    ctx.drawImage(imageElem, 0, 0, canvas.width, canvas.height)
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    let msg = 'secret message hurray!'
+    let key = 'thisismykey2343'
+
+    imageData = embedData(msg, imageData)
+    let hidden = extractData(msg.length * 8 - 1, imageData)
+
+    ctx2.putImageData(imageData, 0, 0)
+    console.log(binStrToText(hidden))
+
+}
+
+function binStrToText(binStr: string): string {
+    let txt = ''
+    for( let i = 0; i < binStr.length; i += 8) {
+        const char = String.fromCharCode(parseInt(binStr.slice(i, i + 8), 2))
+        txt += char
+    }
+    return txt
+
+}
+
+function extractData(msgLen: number, img: ImageData): string {
+    let msgBin = ''
+    let channelIdx = 0
+    let channelCtr = 0
+
+    for( let i = 0; i < msgLen; i++) {
+
+        let channel = img.data[channelIdx].toString(2).padStart(8, '0')
+        
+        if ( channel[6] === '1' && channel[7] === '1') {
+            msgBin += '0'
+        } else {
+            msgBin += '1'
+        }
+
+        if( channelCtr === 2) {
+            channelIdx+=2
+            channelCtr = 0
+        } else {
+            channelIdx++
+            channelCtr++
+        }
+    }
+    
+    return msgBin
+}
+
+function embedData(msg: string, img: ImageData): ImageData {
+    // bytes array 
+    let msgBin: number[] = []
+    for( let i = 0; i < msg.length; i++) {
+        msgBin.push(msg.charCodeAt(i))
+    }
+
+    let channelIdx = 0
+    let channelCtr = 0
+
+    for( let i = 0; i < msgBin.length; i++) {
+        let toProc = msgBin[i]
+        // extract 8 bits of the msg byte 
+        let msgBitCarrier = ''
+        for( let j = 0; j < 8; j++) {
+            msgBitCarrier = toProc.toString(2).padStart(8, '0')
+            msgBitCarrier = msgBitCarrier[j] // go from MSB to LSB
+
+            let channel = img.data[channelIdx].toString(2).padStart(8, '0')
+            let ch7thBit = parseInt(channel[6], 2)
+            
+            // XOR 7th bit of the image rgb component with the messsage bit 
+            let xorRes = ch7thBit ^ parseInt(msgBitCarrier, 2)
+            
+            // place result in 8th bit of the pixel channel
+            let binRepr: string | string[] = img.data[channelIdx].toString(2).padStart(8, '0')
+            binRepr = binRepr.split('')
+            binRepr.pop()
+            binRepr.push(xorRes.toString(2))
+            binRepr = binRepr.join('')
+            
+            img.data[channelIdx] = parseInt(binRepr, 2)
+            
+            if ( channelCtr === 2) {
+                channelIdx += 2
+                channelCtr = 0
+                
+            } else {
+                channelIdx++
+                channelCtr++
+            }  
+        }
+    }
+    
+    return img 
+}
+
+
+function main() {
+    let message = '123'
+    let img = new Uint8Array(4 * 20).fill(255)
+
+    // bytes array 
+    let msgBin: number[] = []
+    for( let i = 0; i < message.length; i++) {
+        msgBin.push(message.charCodeAt(i))
+    }
+
+    let channelIdx = 0
+    let channelCtr = 0
+
+    for( let i = 0; i < msgBin.length; i++) {
+        let toProc = msgBin[i]
+        // extract 8 bits of the msg byte 
+        let msgBitCarrier = ''
+        for( let j = 0; j < 8; j++) {
+            msgBitCarrier = toProc.toString(2).padStart(8, '0')
+            msgBitCarrier = msgBitCarrier[j] // go from MSB to LSB
+
+            let channel = img[channelIdx].toString(2).padStart(8, '0')
+            let ch7thBit = parseInt(channel[6], 2)
+            
+            // XOR 7th bit of the image rgb component with the messsage bit 
+            let xorRes = ch7thBit ^ parseInt(msgBitCarrier, 2)
+            
+            // place result in 8th bit of the pixel channel
+            let binRepr: string | string[] = img[channelIdx].toString(2).padStart(8, '0')
+            binRepr = binRepr.split('')
+            binRepr.pop()
+            binRepr.push(xorRes.toString(2))
+            binRepr = binRepr.join('')
+            
+            img[channelIdx] = parseInt(binRepr, 2)
+            
+            if ( channelCtr === 2) {
+                channelIdx += 2
+                channelCtr = 0
+                
+            } else {
+                channelIdx++
+                channelCtr++
+            }  
+        }
+    }
+
+
+    let msgBinRepr = ''
+    for( let char of message) {
+        msgBinRepr += char.charCodeAt(0).toString(2).padStart(8, '0')
+    }
+    console.log('Message Bin Representation :')
+    console.log(msgBinRepr)
+    console.log('---------------------------------------')
+
+    for( let channel of img) {
+        console.log(channel.toString(2).padStart(8, '0'))
+    }
+
+}
